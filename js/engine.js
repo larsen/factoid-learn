f8b774b2f62e5e4173305f3a2612ea95aaf85431
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2010-07-23 17:43:07 +0100

    Added score and counter; some refactoring

--- js/engine.js
+++ js/engine.js
@@ -1,4 +1,6 @@
 var state_to_guess;
+var score = 0;
+var questions = 0;
 
 function my_debug( string ) {
     // alert( string );
@@ -6,11 +8,27 @@ function my_debug( string ) {
 
 function pick( map ) {
 
-    var keys = new Array();
+    var keys = [];
     $.each( factoids, function( key, value ) { keys.push( key ) } )
     var l = keys.length;
 
     return keys[ Math.floor(Math.random()*l +1) ];
+}
+
+function update_score() {
+    $('#score').html( score );
+    $('#questions').html( questions );
+}
+
+function new_game() {
+    my_debug( "here" );
+    $('#answer').val("");
+    $('#evaluation').html("");
+    state_to_guess = pick( factoids );
+    $('#state').html( state_to_guess );
+    $('#answer').focus();
+    questions++;
+    update_score();
 }
 
 $(document).ready( 
@@ -19,12 +37,7 @@ $(document).ready(
 
         $("#new_game").click( 
             function() {
-                my_debug( "here" );
-                $('#answer').val("");
-                $('#evaluation').html("");
-                state_to_guess = pick( factoids );
-                $('#state').html( state_to_guess );
-                $('#answer').focus();
+                new_game();
             }
         );
 
@@ -35,13 +48,16 @@ $(document).ready(
 
                 if ( answer == factoids[ state_to_guess ] ) {
                     $("#evaluation").html( "YEAH" );
+                    score++;
                 }
                 else {
                     $("#evaluation").html( "No, the correct answer was " 
                         + factoids[ state_to_guess ] );
                 }
+                $("#new_game").focus();
             }
         );
 
+        new_game();
     }
 );
